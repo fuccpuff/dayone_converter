f8980b895474b9f7238537cc10e3623ff9c29222
Pavel Larionov <fuccpuff@gmail.com>
2020-12-14 13:09:25 +0500

    add convert void

--- app/src/main/java/com/example/dayone_convertor/MainActivity.java
+++ app/src/main/java/com/example/dayone_convertor/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.dayone_convertor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void convert(View view) {
+        EditText dollarValues = findViewById(R.id.dollarValues);
+        TextView rubValues = findViewById(R.id.rubValues);
+
+        if (dollarValues.getText().toString().equals("")) {
+            rubValues.setText(R.string.no_value);
+        } else {
+            int dollars =  Integer.parseInt(dollarValues.getText().toString());
+            double ruble = dollars * 73.12;
+            rubValues.setText(ruble + "");
+        }
     }
 }
