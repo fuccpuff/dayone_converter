3503a45525319e20767bebe11543cade99a2bbbe
Pavel Larionov <fuccpuff@gmail.com>
2020-12-14 15:36:35 +0500

    again

--- app/src/main/java/com/example/dayone_convertor/MainActivity.java
+++ app/src/main/java/com/example/dayone_convertor/MainActivity.java
@@ -11,20 +11,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
+
     public static final String TAG = "stateChange";
-    private static final String RUBLE_VALUE = "rubValues";
+    public static final String RUBLE_VALUE = "rubValues";
 
     TextView rubValues;
-    
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         rubValues = findViewById(R.id.rubValues);
-        
+
         Log.i(TAG, "onCreate()");
     }
 
@@ -32,13 +31,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "onStart()");
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "onStop()");
     }
 
     @Override
@@ -52,19 +44,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Log.i(TAG, "onResume()");
+
     }
 
     @Override
-    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
         outState.putString(RUBLE_VALUE, rubValues.getText().toString());
         Log.i(TAG, "onSaveInstanceState()");
+
+
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop()");
     }
 
     @Override
@@ -80,13 +82,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void convert(View view) {
-        
+
         EditText dollarValues = findViewById(R.id.dollarValues);
 
         if (dollarValues.getText().toString().equals("")) {
             rubValues.setText(R.string.no_value);
         } else {
-            int dollars =  Integer.parseInt(dollarValues.getText().toString());
+            int dollars = Integer.parseInt(dollarValues.getText().toString());
             double ruble = dollars * 73.12;
             rubValues.setText(ruble + "");
         }
